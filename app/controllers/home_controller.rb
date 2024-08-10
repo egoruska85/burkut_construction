2a7082bec588503f3ef3e020531a61235e0db8c6
Egor Arbuzov <egorarbuzov@gmail.com>
2024-08-10 17:58:54 +0300

    navbar

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,12 @@
 class HomeController < ApplicationController
   def index
+    #@headers = Header.all
+
+    Header.all.each { |header| @header = header }
+
+    @type = @header.type_ru if params[:locale] != true and params[:locale] != 'en' and params[:locale] != 'tm' and params[:locale] != 'ru'
+    @type = @header.type_ru if params[:locale] == 'ru'
+    @type = @header.type_en if params[:locale] == 'en'
+    @type = @header.type_tm if params[:locale] == 'tm'
   end
 end
